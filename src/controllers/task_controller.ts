097b69d62ab3134b4c3a2e99455b24e0491d4fc4
Dave Charm Bulaquena <davecharm16@gmail.com>
2024-10-26 09:44:05 +0800

    feat: added update task by author or assigned user

--- src/controllers/task_controller.ts
+++ src/controllers/task_controller.ts
@@ -1,5 +1,5 @@
-import { validateTask } from '../core/utils/utils.js';
-import { Request, Response, } from 'express';
+import { validateTask, validateUpdateTask } from '../core/utils/utils.js';
+import e, { Request, Response, } from 'express';
 import { Task } from '../models/task/schema/task_schema.js';
 import { Types } from 'mongoose';
 export const getAllTaskController = async (req: Request, res: Response)  => {
@@ -14,12 +14,13 @@ export const getAllTaskController = async (req: Request, res: Response)  => {
     res.status(500).json({error: 'Error getting task'});
   }
 }
-export const getTaskByUserIdController = async (req: Request, res: Response) => {
+export const getTaskByAssignedUserIdController = async (req: Request, res: Response) => {
   try{
     const userId = req.params?.id;
     const tasks = await Task.find({
       'assigned_to.user': new Types.ObjectId(userId),
     })
+    .populate({path: 'author', select : '-password -email -tokens -createdAt'})
     .populate({path: 'assigned_to.user', select : '-password -email -tokens -createdAt'});
     res.status(200).json(tasks);
   }
@@ -36,6 +37,7 @@ export const getTaskByAuthorIdController = async (req: Request, res: Response) =
     const tasks = await Task.find({
       author: new Types.ObjectId(userId),
     })
+    .populate({path: 'author', select : '-password -email -tokens -createdAt'})
     .populate({path: 'assigned_to.user', select : '-password -email -tokens -createdAt'});
     res.status(200).json(tasks);
   }
@@ -59,15 +61,51 @@ export const createTaskController = async (req: Request, res: Response) => {
     res.status(500).json(e);
   }
 }
-export const updateTaskController = async (req: Request, res: Response) => {
 
+export const updateTaskController = async (req: Request, res: Response) => {
+  try{
+    const taskId = req.params.id;
+    const userId = req.body.userId;
+
+    if (!Types.ObjectId.isValid(taskId) || !Types.ObjectId.isValid(userId)) {
+      res.status(400).json({ error: 'Invalid task or user ID format' });
+    }
+    else{
+      const { userId: _, ...updates } = req.body;
+      const validate = await validateUpdateTask(updates);
+      const updatedTask = await Task.findOneAndUpdate(
+        {
+          _id : taskId,
+          $or : [
+            {'assigned_to.user': new Types.ObjectId(userId)},
+            {'author': new Types.ObjectId(userId)},
+          ]
+        },
+        { $set: updates },       
+        { new: true, runValidators: true }
+      )
+      .populate({path: 'author', select : '-password -email -tokens -createdAt'})
+      .populate({path: 'assigned_to.user', select : '-password -email -tokens -createdAt'});
+      ;
+      
+      if (!updatedTask) {
+        res.status(404).json({ error: 'Task not found' });
+      }
+      else{
+        res.status(200).json(updatedTask);
+      }
+    }
+  }
+  catch(e){
+    res.status(400).json(e);
+  }
 }
 
 export const deleteTaskController = async (req: Request, res: Response) => {
   try {
-    const task_id = req.params?.id;
-    const user_id = req.body.user_id;
-    const task = await Task.findOneAndDelete({_id : task_id, author : user_id});
+    const taskId = req.params?.id;
+    const userId = req.body.userId;
+    const task = await Task.findOneAndDelete({_id : taskId, author : userId});
     if(!task){
        res.status(400).json({error: 'Cannot Delete this'});
     }else{
